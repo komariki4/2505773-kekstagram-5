ed1317265d02a9dab1fde2916d1aa3bc1beb2293
Вероника Борисова <veronikab0104@gmail.com>
2024-09-29 00:35:11 +0500

    Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -2,8 +2,17 @@ function checkMaxString (string, maxLength) {
   return string.length <= maxLength;
 }
 
-function isPalindromeString(string) {
+
+checkMaxString('проверяемая строка', 20);
+checkMaxString('проверяемая строка', 18);
+checkMaxString('проверяемая строка', 10);
+
+function isPalindromeString() {
   const newString = String.toLowerCase();
   return newString === newString.split('').reverse().join('');
 }
 
+
+isPalindromeString('топот');
+isPalindromeString('ДовОд');
+isPalindromeString('Кекс');
