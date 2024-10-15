0f3096a9e287ede1039d34dd746810fdf958b5b9
Вероника Борисова <veronikab0104@gmail.com>
2024-10-15 17:13:11 +0500

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,22 @@
-function checkMaxString (string, maxLength) {
-  return string.length <= maxLength;
+function parseTime(timeString) {
+  const [hour, minute] = timeString.split(':');
+  return hour * 60 + Number(minute);
+}
+
+function chekMeeting(dayStart, dayEnd, meetingStart, meetingDuration) {
+  const dayStartInMinutes = parseTime(dayStart);
+  const dayEndInMinutes = parseTime(meetingStart);
+  const meetingStartInMinutes = parseTime(meetingStart);
+
+  return (
+    meetingStartInMinutes >= dayStartInMinutes &&
+    meetingStartInMinutes + meetingDuration <= dayEndInMinutes
+  );
 }
 
 
-checkMaxString('проверяемая строка', 20);
-checkMaxString('проверяемая строка', 18);
-checkMaxString('проверяемая строка', 10);
-
-function isPalindromeString() {
-  const newString = String.toLowerCase();
-  return newString === newString.split('').reverse().join('');
-}
-
-
-isPalindromeString('топот');
-isPalindromeString('ДовОд');
-isPalindromeString('Кекс');
+chekMeeting('08:00', '17:30', '14:00', 90); 
+chekMeeting('8:0', '10:0', '8:0', 120);     
+chekMeeting('08:00', '14:30', '14:00', 90); 
+chekMeeting('14:00', '17:30', '08:0', 90);  
+chekMeeting('8:00', '17:30', '08:00', 900); 
